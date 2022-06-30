0ba385e2664f898cb5945f0a54f0e417b32a532a
Santiago <santiagomarinocampo@gmail.com>
2022-06-29 22:19:06 -0500

    Version 2

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20221/lab1/personalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20221/lab1/personalDataActivity.kt
@@ -3,19 +3,38 @@ package co.edu.udea.compumovil.gr04_20221.lab1
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
 
 
-
-class personalDataActivity: AppCompatActivity() {
+class personalDataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.personal_data_activity)
-        val items = listOf("Material", "Design", "Components", "Android")
+        val items = resources.getStringArray(R.array.array_español_school_grade)
         val adapter = ArrayAdapter(this, R.layout.list_item, items)
         val auto = findViewById<AutoCompleteTextView>(R.id.autoComplete)
         auto.setAdapter(adapter)
+        val textfieldDate = findViewById<TextInputEditText>(R.id.editDataOfBirth)
+        val btnChangeDate = findViewById<Button>(R.id.btnDateOfbirth)
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+        btnChangeDate.setOnClickListener {
+            datePicker.show(supportFragmentManager, "tag");
+            datePicker.addOnPositiveButtonClickListener {
+                textfieldDate.setText(datePicker.headerText)
+            }
+        }
+
+
 
     }
 }
